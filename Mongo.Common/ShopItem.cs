d4788100c5e65f6f121a19e2d8d14d1adcf44451
SmackyPappelroy <65800614+SmackyPappelroy@users.noreply.github.com>
2023-01-19 22:07:22 +0100

    Refinements

--- Mongo.Common/ShopItem.cs
+++ Mongo.Common/ShopItem.cs
@@ -2,6 +2,7 @@
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.IdGenerators;
 using System.Reflection.Emit;
+using System.Text.Json.Serialization;
 
 namespace Mongo.Common;
 
@@ -9,11 +10,14 @@ public class ShopItem : IEntity
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
+    [JsonPropertyName("_id")]
     public string Id { get; set; }
 
     [BsonElement("item")]
+    [JsonPropertyName("item")]
     public string Item { get; set; }
 
     [BsonElement("price")]
+    [JsonPropertyName("price")]
     public int Price { get; set; }
 }
--- Mongo.UI/Program.cs
+++ Mongo.UI/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Mongo.Common;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://localhost:7118/api/ShopItems");
+request.Method = "GET";
+//specify other request properties
+
+try
+{
+    var response = (HttpWebResponse)request.GetResponse();
+    using (var streamReader = new StreamReader(response.GetResponseStream()))
+    {
+        var json = streamReader.ReadToEnd();
+        List<ShopItem> shop = JsonSerializer.Deserialize<List<ShopItem>>(json);
+    }
+}
+catch (Exception ex) { }
